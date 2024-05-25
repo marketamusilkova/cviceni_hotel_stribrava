3042de7ade8f86d48a6c22fd6ec7fb077c259a77
Pavlína Krátká <pajus.kratka@gmail.com>
2024-05-25 14:43:27 +0200

    zprovoznění react routeru

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,38 @@
 import { createRoot } from 'react-dom/client';
 import { HomePage } from './pages/HomePage';
 import './global.css';
+import { AdminPage } from './components/AdminPage';
+import React from 'react';
+import {createBrowserRouter, Outlet, RouterProvider} from 'react-router-dom'
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+
+const App = () => (<>
+  <Header />
+  <Outlet />
+  <Footer />
+  </>
+)
+  
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+        
+      },
+      {
+        path: "/admin",
+        element: <AdminPage />,
+        
+      }
+    ]
+  }
+])
 
 createRoot(
   document.querySelector('#app'),
-).render(<HomePage />);
+).render(<RouterProvider router={router} />);
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,14 +1,11 @@
-import { Detail } from "../../components/Detail";
-import { Footer } from "../../components/Footer";
-import { Header } from "../../components/Header";
 import { Rooms } from "../../components/Rooms";
 
 export const HomePage = () => {
   return (
   <div>
-<Header />
+
 <Rooms />
-<Footer />
+
 
   </div>)
 };
